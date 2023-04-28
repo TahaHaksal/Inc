2a8568048bed8f8ed809d1e5ecb3250b06a626f0
Taha <m.haksal@gmail.com>
2023-04-28 18:17:19 +0300

    modified:   srcs/db/Dockerfile 	modified:   srcs/db/tools/db.sh 	modified:   srcs/db/tools/init.sql

--- srcs/db/tools/db.sh
+++ srcs/db/tools/db.sh
@@ -2,11 +2,13 @@
 
 service mysql start;
 
-sed -i 's/username_placeholder/'${MYSQL_USER}'/g' init.sql && \
-sed -i 's/password_placeholder/'${MYSQL_PASSWORD}'/g' init.sql && \
-sed -i 's/database_placeholder/'${MYSQL_DB}'/g' init.sql
 
+
+sed -i "s/username_placeholder/${MYSQL_USER}/g" init.sql && \
+sed -i "s/password_placeholder/${MYSQL_PASSWORD}/g" init.sql && \
+sed -i "s/database_placeholder/${MYSQL_DB}/g" init.sql
+cat init.sql
 mysql < init.sql
 
-mysqladmin -u root shutdown;
-rm -f init.sql;
+# mysqladmin -u root shutdown;
+# rm -f init.sql;
--- srcs/db/tools/init.sql
+++ srcs/db/tools/init.sql
@@ -1,4 +1,4 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 CREATE DATABASE IF NOT EXISTS database_placeholder;
 CREATE USER IF NOT EXISTS 'username_placeholder'@'%' IDENTIFIED BY 'password_placeholder';
 GRANT ALL PRIVILEGES ON database_placeholder.* TO 'username_placeholder'@'%';
